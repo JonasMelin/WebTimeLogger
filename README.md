5653731d433e7248880aefc9f83b4ab5c3f59907
Jonas Melin <jonas.melin@live.com>
2022-10-17 10:36:58 +0200

    Project runnable out of the box after cloning

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # WebTimeLogger
 Web based time logging tool where you can log where and how much time you spend. Work in progress...
+
+# HowTo develop
+ * Start MariaDb in a container: ./startMariaDb.sh
+ * Start the backend Restserver in your ide:
+   * Open project in IntelliJ or similar
+   * Run file "MainApplication.kt"
+   * Start do POST/GET REST requests according to controller classes
+
+
--- startMariaDb.sh
+++ startMariaDb.sh
@@ -1,3 +1,9 @@
-docker run --network=host -d --rm -v /home/jonas/Documents/kotlin2/demo2/mariadbdata:/var/lib/mysql   --env MARIADB_ALLOW_EMPTY_ROOT_PASSWORD=true  mariadb
+
+mariadbpath=$(pwd)/mariadbdata
+
+mkdir -p $mariadbpath
+
+
+docker run --network=host -d --rm -v ${mariadbpath}:/var/lib/mysql   --env MARIADB_ALLOW_EMPTY_ROOT_PASSWORD=true  mariadb
 
 #mariadb --host 127.0.0.1 --port 3306 --user root
