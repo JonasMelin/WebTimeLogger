e138809afb92e8f8b7a0be659fbd661df5be6873
jonas <jonas.melin@live.com>
2022-10-19 10:50:59 +0200

    mariadb path set to usb disk

--- startMariaDb.sh
+++ startMariaDb.sh
@@ -1,9 +1,9 @@
 
-mariadbpath=$(pwd)/mariadbdata
+#mariadbpath=$(pwd)/mariadbdata
+mariadbpath=/media/jonas/USBMAIN2TB/diverse/mariadb
 
 mkdir -p $mariadbpath
 
 
 docker run --network=host -d --rm -v ${mariadbpath}:/var/lib/mysql   --env MARIADB_ALLOW_EMPTY_ROOT_PASSWORD=true  mariadb
 
-#mariadb --host 127.0.0.1 --port 3306 --user root
